005844cca0cdc16cf060ab0ed498bf87f60b2409
musadek akhmad <musadekakhmad@gmail.com>
2025-09-13 06:59:33 +0700

    Update page.js

--- app/genre/[mediaType]/[id]/page.js
+++ app/genre/[mediaType]/[id]/page.js
@@ -2,7 +2,7 @@ import MovieCard from '@/components/MovieCard';
 import { notFound } from 'next/navigation';
 
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-const API_URL = process.env.NEXT_PUBLIC_TMDB_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_TMDB_API_URL;
 
 // Fungsi untuk mengambil data genre dari API
 async function getGenre(mediaType) {
