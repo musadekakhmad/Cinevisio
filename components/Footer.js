86e52d3f3a8ac624abad8292fcc71ddafaa1eccf
musadek akhmad <musadekakhmad@gmail.com>
2025-09-13 06:08:34 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bg-gray-800 text-center text-gray-400 p-4 mt-12">
-    <p>© 2025 Cine Visio. All rights reserved.</p>
-	<p>Real Time Update Movies and Tv Shows TMDB</p>
+    <p>© 2025 <a href="https://cinevisio.netlify.app/" className="text-blue-500 hover:text-blue-400 transition-colors duration-200">Cine Visio</a>. All rights reserved.</p>
+    <p>Real Time Update Movies and Tv Shows by <a href="https://www.themoviedb.org/" className="text-blue-500 hover:text-blue-400 transition-colors duration-200">TMDB</a></p>
   </footer>
 );
 
